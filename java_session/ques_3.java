5498f05cf884c297fc87d9af38340c3430d5e1a2
ashutosh <ashutosh.srivastava@tothenew.com>
2020-02-19 22:50:16 +0530

    java exercise 2

--- java_session/ques_3.java
+++
@@ -1,9 +0,0 @@
-package java_session;
-
-public class ques_3 {
-    public static void main(String[] args) {
-        String string = "ashutosh srivastava";
-        String ch = "a";
-        System.out.println(string.length() - string.replace(ch,"").length());
-    }
-}
