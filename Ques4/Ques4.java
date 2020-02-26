f6eb006b6183af1458cb4544b812a4dd4a0c2eb2
ashuTTN <60733861+ashuTTN@users.noreply.github.com>
2020-02-26 20:54:15 +0530

    Update Ques4.java

--- Ques4/Ques4.java
+++ Ques4/Ques4.java
@@ -1,16 +1,40 @@
 package Ques4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-//Write a program to sort hashmaps by values.
-public class Ques4{
+import java.util.Map.Entry;
+import java.util.TreeMap;
+public class Ques4 {
     public static void main(String[] args) {
-        Map<String,Integer> map = new HashMap<>();
-        map.put("Ashutosh",34);
-        map.put("Anupam",56);
-        map.put("Subarno",7);
-        map.put("chirag",100);
-        map.put("lakshya",43);
+        HashMap<String, String> Map = new HashMap<>();
+        LinkedHashMap<String, String> sortedMap = new LinkedHashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        Map.put("5", "Ashutosh");
+        Map.put("8", "Anupam");
+        Map.put("3", "Sub");
+        Map.put("7", "lada");
+        Map.put("2", "hooe");
 
+        for (Map.Entry<String, String> entry : Map.entrySet()) {
+            list.add(entry.getValue());
+        }
+
+        Collections.sort(list, new Comparator<String>() {
+            public int compare(String str, String str1) {
+                return (str).compareTo(str1);
+            }
+        });
+        for (String str : list) {
+            for (Entry<String, String> entry : Map.entrySet()) {
+                if (entry.getValue().equals(str)) {
+                    sortedMap.put(entry.getKey(), str);
+                }
+            }
+        }
+        System.out.println(sortedMap);
     }
 }
