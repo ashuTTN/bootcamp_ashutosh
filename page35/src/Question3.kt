29e1918540b4163f4f98bfc748f36efc75d9f302
ashutosh <ashutosh.srivastava@tothenew.com>
2020-05-07 09:10:17 +0530

    Adding missing quesitons

--- page35/src/Question3.kt
+++ page35/src/Question3.kt
@@ -2,19 +2,26 @@
 //write a function which will have base
 //class object as an argument and it will return name of the subclass based argument type.
 
-sealed class University{
-
-}
-class Student:University(){
-
-}
-class Teacher:University(){
-
-}
-class Staff:University(){
-
+sealed class Base{
+    class A(val a:Int):Base()
+    class B(val b: Int,val c:Int):Base()
+    class C(val a:Int,val b: Int,val c:Int):Base()
 }
 
-fun aFunction(universityObject: University){
-    return 
+fun evaluate(e:Base){
+    when(e){
+        is Base.A -> println("Subclass A called")
+        is Base.B -> println("Subclass B called")
+        is Base.C -> println("Subclass C called")
+    }
+}
+
+fun main(){
+    val objA = Base.A(2)
+    val objB = Base.B(2,4)
+    val objC = Base.C(2,5,8)
+
+    evaluate(objA)
+    evaluate(objB)
+    evaluate(objC)
 }
